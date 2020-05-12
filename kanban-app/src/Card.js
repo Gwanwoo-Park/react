b22335f83d3c0642a6b2d57c03243370f0c14dbb
박관우 <pgw9470@naver.com>
2020-05-12 14:46:28 +0900

    kanaban add task compeleted

--- kanban-app/src/Card.js
+++ kanban-app/src/Card.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import TaskList from './TaskList';
 import PropTypes from 'prop-types';
@@ -6,36 +5,37 @@ import PropTypes from 'prop-types';
 import styles from './Card.css';
 
 export default class Card extends React.Component {
-    constructor() {
+    constructor(){
         super(...arguments);
         this.state = {
             showDetails: false
         }
     }
-
-    onTitleClick() {
-        this.setState({ 
-            showDetails: !this.state.showDetails 
+    
+    onTitleClick(event){
+        this.setState({
+            showDetails: !this.state.showDetails
         });
     }
 
     render() {
         return (
             <div className={ styles.Card }>
-                <div className={ styles[`SideBar--${ this.props.status }`] } />
-                <div 
-                    className={ styles[this.state.showDetails ? 'Title--is-open' : 'Title'] }
+                <div className={ styles[`SideBar--${this.props.status}`] } />
+                <div
+                    className={ styles[this.state.showDetails ? 'Title--is-open' : 'Title'] } 
                     onClick={ this.onTitleClick.bind(this) }>
                     { this.props.title }
                 </div>
                 { !this.state.showDetails ? null : <div>
-                    { this.props.description }
-                    <TaskList 
-                        key = { this.props.id }
-                        cardId = { this.props.id }
-                        tasks = { this.props.tasks } />
+                    {this.props.description }
+                    <TaskList
+                        key={ this.props.id }
+                        cardId={ this.props.id } 
+                        tasks={ this.props.tasks } 
+                        taskCallbacks={ this.props.taskCallbacks } />
                 </div> }
-            </div>
+          </div>
         )
     }
 }
@@ -45,5 +45,5 @@ Card.propTypes = {
     title: (props, propName, component) => (!props[propName] || typeof props[propName] !== 'string' || props[propName].length > 50) ? new Error(`${ propName } in ${ component } is longer than 50 Characters`) : null,
     description: PropTypes.string.isRequired,
     color: PropTypes.string,
-    tasks: PropTypes.arrayOf(PropTypes.object)
+    task: PropTypes.arrayOf(PropTypes.object)
 }
--- kanban-app/src/TaskList.js
+++ kanban-app/src/TaskList.js
@@ -4,23 +4,31 @@ import PropTypes from 'prop-types';
 import styles from './TaskList.css';
 
 export default class TaskList extends React.Component {
+    onInputKeyPress(event) {
+        if(event.key == 'Enter'){
+            this.props.taskCallbacks.add(this.props.cardId, event.target.value);
+            event.target.value = '';
+        }
+    }
+
     render() {
         return (
             <div>
                 <ul>
-                    { this.props.tasks.map(task => <li 
-                        key = { task.id }
-                        className={ styles.Task }>
+                    { this.props.tasks.map(task => <li
+                            key={ task.id } 
+                            className={ styles.Task }>
                             <input type='checkbox' defaultChecked={ task.done } />
                             { task.name }
                             <a href='#' className={ styles['Task--remove'] } />
-                        </li> ) }
+                        </li>) }
                 </ul>
-                <input 
-                    type='text' 
-                    className={ styles['Input--add-task'] } 
-                    placeholder='새테스크' />
-            </div>
+                <input
+                    type='text'
+                    className={ styles['Input--add-task'] }
+                    placeholder='새태스크'
+                    onKeyPress={ this.onInputKeyPress.bind(this) } />
+             </div>
         )
     }
 }
