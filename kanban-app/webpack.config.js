566332cfddef63bff84ab6a9203767c821f31189
박관우 <pgw9470@naver.com>
2020-05-23 08:51:20 +0900

    리액트 연습ㅠㅠ

--- kanban-app/webpack.config.js
+++ kanban-app/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     devServer: {
         contentBase: path.resolve('public'),
         host: '0.0.0.0',
-        port: 9999,
+        port: 9990,
         inline: true,
         liveReload: true,
         hot: false,
